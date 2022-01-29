c0169be058a5c5920f50b1d7656df73c547b6cbb
unknown <unknown@example.com>
2022-01-29 22:04:26 +0800

    1/29
    VM24报错

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,14 +1,45 @@
 <template>
   <view class="index">
+   
+
     <view>
-      <img src="" alt="">
+     <div class="content">
+        <div class="all tras" style="left: 0">
+            <div class="shopshow"><img src="img/1.jpg" alt=""></div>
+            <div class="shopshow"><img src="img/2.jpg" alt=""></div>
+            <div class="shopshow"><img src="img/3.jpg" alt=""></div>
+            <div class="shopshow"><img src="img/4.jpg" alt=""></div>
+            <div class="shopshow"><img src="img/5.jpg" alt=""></div>
+            <div class="shopshow"><img src="img/6.jpg" alt=""></div>
+            <div class="shopshow"><img src="img/1.jpg" alt=""></div>
+        </div>
+        <div class="circle">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        <div class="btn">
+            <div>
+                <</div> <div>>
+            </div>
+        </div>
+    </div>
+ 
+　　
+      
     </view>
     {{ msg }}
     <view class="btn">
       <nut-button type="primary" @click="handleClick('text', msg2, true)">点我</nut-button>
     </view>
     <nut-toast :msg="msg" v-model:visible="show" :type="type" :cover="cover" />
+    
+   
   </view>
+   
 </template>
 
 <script>
@@ -17,6 +48,37 @@ export default {
   name: 'Index',
   components: {
     
+        var all = document.getElementsByClassName("all")[0];
+        var shopshow = document.getElementsByClassName("shopshow");
+        var content = document.getElementsByClassName("content")[0];
+        var count = 0; 
+        var img = document.getElementsByTagName("img")[0];
+        var length = parseInt(window.getComputedStyle(img).width);
+        var speed = 1000; 
+        var circle = document.getElementsByClassName("circle")[0].children;
+        var index = 0; 
+        var timer = null; 
+        var btn = document.getElementsByClassName("btn")[0].children;
+
+
+        function move() {
+            all.className = "all tras";
+            count += 1;
+            all.style.left = -(count * length) + "px";
+            
+            setTimeout(function () {
+            
+                if (count >= shopshow.length - 1) {
+                    all.className = "all";
+                    count = 0;
+                    all.style.left = "0px";
+                }
+                
+                index = count;
+                addcolor();
+            }, 500);
+        }
+
   },
   setup(){
     const state = reactive({
@@ -39,6 +101,7 @@ export default {
       handleClick
     }
   }
+
 }
 </script>
 
@@ -48,5 +111,96 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+   
 }
+* {
+            padding: 0;
+            margin: 0;
+        }
+
+        .content {
+            width: 400px;
+            height: 300px;
+            margin: auto;
+            border: solid 1px gray;
+            overflow: hidden;
+            position: relative;
+        }
+
+        .all {
+            width: 2800px;
+            height: 300px;
+            position: absolute;
+        }
+
+        .shopshow {
+            width: 400px;
+            height: 300px;
+            float: left;
+        }
+
+        img {
+            width: 100%;
+            height: 100%;
+        }
+
+        .tras {
+            transition: left 0.5s ease-in;
+        }
+
+        .circle {
+            width: 120px;
+            height: 12px;
+            font-size: 0;
+            line-height: 12px;
+            position: absolute;
+            bottom: 20px;
+            left: 140px;
+        }
+
+        .circle>div {
+            border: solid 2px #fff;
+            border-radius: 50%;
+            width: 8px;
+            height: 8px;
+            display: inline-block;
+            margin: 0 4px;
+        }
+
+        .bgc {
+            background-color: #fff;
+        }
+
+        .btn {
+            position: absolute;
+            width: 100%;
+            height: 30px;
+            top: 125px;
+            left: 0;
+        }
+
+        .btn>div:nth-child(1) {
+            width: 30px;
+            height: 30px;
+            background-color: #e2e2e2;
+            border-radius: 0 50% 50% 0;
+            text-align: center;
+            line-height: 30px;
+            float: left;
+            font-size: 18px;
+            color: #a89c9c;
+        }
+
+        .btn>div:nth-child(2) {
+            width: 30px;
+            height: 30px;
+            background-color: #e2e2e2;
+            border-radius: 50% 0 0 50%;
+            text-align: center;
+            line-height: 30px;
+            float: right;
+            font-size: 18px;
+            color: #a89c9c;
+        }
+
 </style>
