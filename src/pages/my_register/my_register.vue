9e680cb7798ae10fa9b1b17bf6b963db05b76803
unknown <unknown@example.com>
2022-02-07 14:20:10 +0800

    进行了注册界面组件的调整位置
    先保存下

--- src/pages/my_register/my_register.vue
+++ src/pages/my_register/my_register.vue
@@ -1,11 +1,26 @@
 <template>
-  <div>
-    <h1>注册</h1>
+  <div id="xingming">
     <form>
       <input type = "text" required placeholder="请输入姓名" class = "input">
+    </form>
+  </div>
+  <div id="xuehao">
+    <form>
       <input type = "text" required placeholder="请输入学号" class = "input">
+    </form>
+  </div>
+  <div id="shouji">
+    <form>
       <input type = "text" required placeholder="请输入手机号" class = "input">
+    </form>
+  </div>
+  <div id="mima">
+    <form>  
       <input type = "text" required placeholder="请输入密码" class = "input">
+    </form>
+  </div>
+  <div id="yanzheng">
+    <form>
       <input type = "text" required placeholder="请输入验证码" class = "input">
       <button type = "submit">注册</button>
     </form>
@@ -34,10 +49,20 @@ export default {
 
 <style lang="scss">
 .input {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)
+  font-size:16px;
+  text-align: left;
+  margin-top: 10px;
+  margin-bottom:10px;
+  margin-left:45px;
+}
+div{
+  margin-left:23px;
+  margin-right:12px;
+  margin-bottom:13px;
+  border: 1px solid black;
+  border-radius: 3px;
+}
+#yanzheng{
+  margin-right:192px;
 }
 </style>
