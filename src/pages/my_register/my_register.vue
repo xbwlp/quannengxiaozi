89b000df3197881fb8b6b5827bb11b7dcd2591d3
unknown <unknown@example.com>
2022-02-09 17:58:02 +0800

    注册界面还差五个小图标，没有图标文件

--- src/pages/my_register/my_register.vue
+++ src/pages/my_register/my_register.vue
@@ -2,34 +2,26 @@
   <h1>
   </h1>
   <div id="tupian">
-    <img src="../../img/a.png">
   </div>
-  <div id="xingming">
-    <form>
+  <form>
+    <div id="xingming">
       <input type = "text" required placeholder="请输入姓名" class = "input">
-    </form>
-  </div>
-  <div id="xuehao">
-    <form>
+    </div>
+    <div id="xuehao">
       <input type = "text" required placeholder="请输入学号" class = "input">
-    </form>
-  </div>
-  <div id="shouji">
-    <form>
+    </div>
+    <div id="shouji">
       <input type = "text" required placeholder="请输入手机号" class = "input">
-    </form>
-  </div>
-  <div id="mima">
-    <form>  
+    </div>
+    <div id="mima"> 
       <input type = "text" required placeholder="请输入密码" class = "input">
-    </form>
-  </div>
-  <div id="yanzheng">
-    <form>
+    </div>
+    <div id="yanzheng">
       <input type = "text" required placeholder="请输入验证码" class = "input">
-      <button type = "submit">注册</button>
-    </form>
-  </div>
+    </div>
+    <button type="submit" id="anniu">获取验证码</button>
+  </form>
+  <button type="submit" id="zhuce">注册</button>
 </template>
  
 <script>
@@ -58,7 +50,7 @@ export default {
   text-align: left;
   margin-top: 10px;
   margin-bottom:10px;
-  margin-left:45px;
+  margin-left:15px;
 }
 div{
   margin-left:23px;
@@ -71,9 +63,83 @@ div{
   margin-right:192px;
 }
 #tupian{ 
+  border:none;
   margin-top:58px;
   margin-left:96px;
   margin-right:97px;
   margin-bottom:41px;
+  height:164px;
+  background-image: url("../../img/a.png") ;/*设置图片的地址*/
+  background-size:182px 164px;    /*这里是图片的大小*/
+  background-repeat:no-repeat ;  /*让图片不重复*/
+  background-position: 0px 0px;
+  padding-left: 33px; 
+}
+#tu{
+  width:182px;
+  heighth:164px;
+}
+#anniu{
+  position:absolute;
+  margin-right:17px;
+  margin-left:198px;
+  margin-top:-52px;
+  background-color:blue;
+  color:white;
+  font-size:16px;
+  width:160px;
+}
+#white{
+  border:0px;
+  border: 1px solid black;
+  border-radius: 3px;
+}
+#zhuce{
+  margin-top:38px;
+  margin-left:122px;
+  margin-right:121px;
+  width: 132px;
+  height: 51px;
+  border-radius: 15px 15px 15px 15px;
+  color: rgba(245, 34, 45, 100);
+  font-size: 18px;
+  text-align: center;
+  font-family: Arial;
+  border: 5px solid rgba(245, 34, 45, 100);
+}
+#xingming{
+  background-image: url("../../img/activity.png") ;/*设置图片的地址,下面五个id选择器分别设置那五个小图标*/
+  background-size:25px 25px;    /*这里是图片的大小*/
+  background-repeat:no-repeat ;  /*让图片不重复*/
+  background-position: 5px 5px;
+  padding-left: 33px; 
+}
+#xuehao{
+  background-image: url("../../img/activity.png") ;/*设置图片的地址*/
+  background-size:25px 25px;    /*这里是图片的大小*/
+  background-repeat:no-repeat ;  /*让图片不重复*/
+  background-position: 5px 5px;
+  padding-left: 33px; 
+}
+#shouji{
+  background-image: url("../../img/activity.png") ;/*设置图片的地址*/
+  background-size:25px 25px;    /*这里是图片的大小*/
+  background-repeat:no-repeat ;  /*让图片不重复*/
+  background-position: 5px 5px;
+  padding-left: 33px; 
+}
+#mima{
+  background-image: url("../../img/activity.png") ;/*设置图片的地址*/
+  background-size:25px 25px;    /*这里是图片的大小*/
+  background-repeat:no-repeat ;  /*让图片不重复*/
+  background-position: 5px 5px;
+  padding-left: 33px; 
+}
+#yanzheng{
+  background-image: url("../../img/activity.png") ;/*设置图片的地址*/
+  background-size:25px 25px;    /*这里是图片的大小*/
+  background-repeat:no-repeat ;  /*让图片不重复*/
+  background-position: 5px 5px;
+  padding-left: 33px; 
 }
 </style>
