e000a59ec2a626a952ec6ac84a5af9ce399a23a7
unknown <unknown@example.com>
2022-02-10 20:27:40 +0800

    活动主页面和个人主页面

--- src/pages/activity/index.vue
+++ src/pages/activity/index.vue
@@ -1,56 +1,83 @@
 <template>
   <view class="index">
-    <view class="header">
-      <img src="" alt="" />
+  <view   class="header1" >
+      <view class="logo1">
+     <img src="http://astc.syac.xyz/2022/02/07/ed7a3696e8de3.png" alt="标题1 _1_.png" title="标题1 _1_.png" />
+      </view>
+  </view>
       
-    </view>
-<view class="txt">品牌活动</view>
-<view style="width:80px;margin:0 auto;" class="header"></view>
-<!-- 设置长度和居中这样配置style就行 -->
+   
+
+
+<view style="width:80px;margin:0 auto;" class="header"></view> 
+<!-- 设置长度和居中这样配置style就行 --> 
+<!--  
 <scroll-view class="box1" scroll-x>
   <view class="box1con">
-   
- <button
- style="width: 260rpx;height: 130rpx;  border-radius: 50rpx;box-shadow: 1px 2px  1px #A9A9A9;"
+-->
+  
+<!--  <button style="width: 260rpx;height: 130rpx; box-shadow: 2px 3px  2px #A9A9A9; border-radius: 50rpx;" 
   size='mini' type='warn' plain @click="go('/pages/activity_sys/activity_sys')">SYS·博雅学堂</button>
-  <!-- 我在这里写了一个阴影box-shadow,不太清楚你在问题里说的按钮边框具体指哪些，文字位置设置就是text-align,我看你109行不是会写了吗 -->
-  <button style="width: 260rpx;height: 130rpx;  border-radius: 50rpx;" 
-  size='mini' type='warn' plain @click="go('/pages/activity_hh/activity_hh')">HH·佳片欣赏会</button>
    
+  <button style="width: 260rpx;height: 130rpx; box-shadow: 2px 3px  2px #A9A9A9; border-radius: 50rpx;" 
+  size='mini' type='warn' plain @click="go('/pages/activity_hh/activity_hh')">HH·佳片欣赏会</button>
+-->
+<!--
+ <button class="button1" type='warn' plain @click="go('/pages/activity_sys/activity_sys')">
+ <text class="tilte">SYS·博雅学堂</text></button>
+ <button class="button1" type='warn' plain @click="go('/pages/activity_gic/activity_gic')">GIC·知行课堂</button>
+
  </view>
 </scroll-view>
 
 <scroll-view class="box1" scroll-x>
   <view class="box1con">
    
- <button style="width: 260rpx;height: 130rpx;  border-radius: 50rpx;" 
+ <button style="width: 260rpx;height: 130rpx; box-shadow: 2px 3px  2px #A9A9A9; border-radius: 50rpx;" 
   size='mini' type='warn' plain @click="go('/pages/activity_gic/activity_gic')">GIC·知行课堂</button>
-  <button style="width: 260rpx;height: 130rpx;  border-radius: 50rpx;" 
+  <button style="width: 260rpx;height: 130rpx; box-shadow: 2px 3px  2px #A9A9A9; border-radius: 50rpx;" 
   size='mini' type='warn' plain @click="go('/pages/activity_bb/activity_bb')">BB·生日欢享会</button>
 
  </view>
 </scroll-view>
+-->
+<view class="block">
+  <view class="content-bar">
+  <button class="button1"   @click="go('/pages/activity_sys/activity_sys')">
+ <text class="title1">SYS·博雅学堂</text></button>
+
+<button class="button1"  plain @click="go('/pages/activity_gic/activity_gic')">
+<text class="title1">GIC·知行课堂</text></button>
+
+<button class="button1"  plain @click="go('/pages/activity_hh/activity_hh')">
+ <text class="title1"> HH·佳片欣赏会</text></button>
+
+<button class="button1"  plain @click="go('/pages/activity_bb/activity_bb')">
+  <text class="title1">BB·生日欢享会</text></button>
+
+  </view>
+</view>
 
 
 
+  <view class="logo1">
+    <img src="http://astc.syac.xyz/2022/02/07/a90e29bf36397.png" alt="标题2.png" title="标题2.png" />
+ </view>
 
-
-<view class="txt" >魅力自选</view>
-
-<view class="header"></view>
+<view style="width:80px;margin:0 auto;" class="header"></view> 
+<!-- 设置长度和居中这样配置style就行 --> 
 <scroll-view class="box1" scroll-x>
   <view class="box1con">
    
- <button style="width: 200rpx;height: 130rpx;  border-radius: 50rpx;" 
-  size='mini' type='warn' plain @click="go('/pages/activity1/activity1')">自选1</button>
-  <button style="width: 200rpx;height: 130rpx;  border-radius: 50rpx;" 
-  size='mini' type='warn' plain @click="go('/pages/activity2/activity2')">自选2</button>
-  <button style="width: 200rpx;height: 130rpx;  border-radius: 50rpx;" 
-  size='mini' type='warn' plain @click="go('/pages/activity3/activity3')">自选3</button>
+ <button class="button2"  plain @click="go('/pages/activity1/activity1')">
+ <text class="title2">自选1</text></button>
+  <button class="button2"  plain @click="go('/pages/activity2/activity2')">
+ <text class="title2">自选2</text></button>
+  <button class="button2"  plain @click="go('/pages/activity3/activity3')">
+ <text class="title2"> 自选3</text></button>
 
  </view>
 </scroll-view>
-
 
   </view>
 
@@ -102,12 +129,81 @@ export default {
   text-align: center;
 }
 
-.header{padding:0rpx 200rpx;width:600rpx;height:0;border:1px solid gray;}
+//.header1{padding:0rpx 200rpx;width:600rpx;height:0;border:0px solid gray}
+.header{
+ // left: 29px;
+//top: 198px;
+//width: 317px;
+margin-top: -154px;
+line-height: 20px;
+padding:0rpx 200rpx;width:600rpx;
+//height: NaNpx;
+opacity: 0.41;
+border: 2rpx solid rgba(116, 139, 242, 100);
+}
+//.box1con{display:flex; justify-content: space-between;padding:10rpx 50rpx;}
 
-.box1con{display:flex; justify-content: space-between;padding:10rpx 50rpx;}
-
-.index.box1{text-align:center;padding:10rpx 50rpx; }
+//.index.box1{text-align:center;padding:10rpx 50rpx; }
 
 .txt{ font-size:54rpx;}
+.logo1 image{
 
+  left: -9px;
+top: 36px;
+width: 380px;
+height: 171px;
+}
+.button1{
+  //left: 195px;
+//top: 211px;
+width: 152px;
+height: 94px;
+//line-height: 20px;
+border-radius: 20px;
+background-color:  rgba(248, 109, 117, 100);
+color: rgba(245, 34, 45, 66);
+box-shadow: 0px 2px 6px 0px rgba(0, 0, 0, 0.4);
+border: 1px solid rgba(187, 187, 187, 100);
+margin-bottom:12px;
+}
+.button2{
+ // left: 23px;
+//top: 599px;
+width: 96px;
+height: 96px;
+line-height: 20px;
+border-radius: 20px 20px 20px 20px;
+background-color: rgba(248, 109, 117, 100);
+color: rgba(31, 177, 240, 100);
+font-size: 14px;
+text-align: center;
+box-shadow: 0px 2px 6px 0px rgba(0, 0, 0, 0.4);
+font-family: Arial;
+border: 1px solid rgba(187, 187, 187, 100);
+}
+.block{
+  display:flex;
+  flex-direction: column;
+}
+.content-bar{
+  padding:10rpx 50rpx;
+  background-color:#F5222D 6600% ;
+  display: flex;
+  flex-wrap:wrap;
+  padding-top:0px ;
+}
+.title1{
+  color: white;
+  font-size:14px;
+  font-family: Arial;
+  text-align: left,right,center,justify;
+  //vertical-align:middle;  
+}
+.title2{
+  color: white;
+  font-size:16px;
+  font-family: Arial;
+  text-align: left,right,center,justify;
+  vertical-align:middle;  
+}
 </style>
--- src/pages/my/index.vue
+++ src/pages/my/index.vue
@@ -1,20 +1,51 @@
 <template>
   <view class="index">
-    <view>
-      <img src="" alt="" />
+    
+      
+  <view class="container hdCon">
+      <view class="logo1">  
+  <img src="http://astc.syac.xyz/2022/02/10/f42ccb06002f7.png" alt="微信图片_20220128222205.png" title="微信图片_20220128222205.png" /> 
+      </view> 
+      <view class="touxiang">
+       <img src="http://astc.syac.xyz/2022/02/10/7ae71d3b92c01.png" alt="我的预约.png" title="我的预约.png" />
+ 
+       <text class="name">会飞的鱼儿</text>
+      </view>
   </view>
+ 
+
     <div>
-   
-    <view class=h2>我预约的活动</view>
-    <view class=h2>我的评价</view>
+   <view style="margin:0 auto;" class="header"></view>  
 
-  <view class='button'>
-  <button style="width: 220rpx;height: 130rpx;  border-radius: 50rpx;" 
-  size='mini'  plain@click="go('/pages/my_login/my_login')">点击登录</button>
+   <view class="box1">
+     <view class="logo2">
+<img src="http://astc.syac.xyz/2022/02/10/7ae71d3b92c01.png" alt="我的预约.png" title="我的预约.png" />
+     </view>
+    <text class=h2>我预约的活动</text>
 
-    <button @click="go('/pages/my_login/my_login')">点击登录</button>
-    <button @click="go('/pages/my_register/my_register')">点击注册</button>
+   </view>
+
+<view class="box2">
+  <view class="logo2">
+<img src="http://astc.syac.xyz/2022/02/10/14d0f361a72d7.png" alt="我的评价.png" title="我的评价.png" />
   </view>
+
+    <text class=h2>我的评价</text>
+
+</view>
+
+<!-- 设置长度和居中这样配置style就行 --> 
+<scroll-view class="box3" scroll-x>
+  <view class="box1con">
+   
+ <button  class="button1" @click="go('/pages/my_login/my_login')">
+ <text class="btn">点击登录</text></button>
+  <button class="button1" @click="go('/pages/my_register/my_register')">
+  <text class="btn">点击注册</text></button>
+
+ </view>
+</scroll-view>
+
   
   </div>
   </view>
@@ -61,6 +92,99 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
 }
-.h2{display:flex;color:red;justify-content:start;}
-.button::after{border:1rpx solid; color:red;}
+
+
+
+
+.container{
+  width:750rpx;
+  height:316px;
+  border:1px solid white;
+}
+.logo1{
+  padding-top: 22px;
+  padding-left: 91px;
+}
+.logo1 image{
+width: 284px;
+height: 234px;
+}
+//头像的位置不会设置
+.touxiang {
+display: flex;
+position: absolute;
+padding-left: 44px;
+top: 195px;
+}
+.touxiang image{
+  width: 100px;
+ height: 100px;
+}
+.name{
+  padding-top:64px;
+  padding-left:18px;
+  font-size:20px;
+  color: rgba(16, 16, 16, 100);
+  font-family: AlibabaPuHui-regular;
+  text-align: left;
+  margin-bottom: 21px;
+  //vertical-align:middle;  
+}
+
+.header{ 
+position: relative;
+width: 234px;
+height: NaNpx;
+border: 1px solid rgba(187, 187, 187, 100);
+}
+
+
+.box1{
+  display:flex;
+  padding-left: 16px;
+  padding-top: 28px;
+}
+.box1 image{
+  width: 40px;
+height: 40px;
+}
+.box2{
+  display:flex;
+  padding-left: 16px;
+  padding-top: 94px;
+  padding-bottom: 96px;
+}
+.box2 image{
+  width: 40px;
+  height: 40px;
+}
+.h2{
+color: rgba(245, 34, 45, 100);
+font-size: 28px;
+text-align: left;
+font-family: PMZDBiaoTi-regular;
+}
+
+
+.box1con{ display:flex; 
+justify-content: space-between;
+padding-bottom:27px;
+}
+
+.button1{
+width: 134px;
+border-radius: 20px 20px 20px 20px;
+color: rgba(16, 16, 16, 100);
+border: 4px solid rgba(245, 34, 45, 100);
+
+ //   background-color: white;
+ // color:blacck;
+//border:4px solid red;
+//position:relative ;
+}
+.btn{
+font-size: 14px;
+text-align: center;
+font-family: Arial;
+}
 </style>
