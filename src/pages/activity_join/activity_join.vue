a2c6405e082c8a8aa2be2c6753392d91aa4cb4ac
Monkey shout <3235>
2022-02-10 23:10:40 +0800

    活动报名绑定

--- src/pages/activity_join/activity_join.vue
+++ src/pages/activity_join/activity_join.vue
@@ -1,7 +1,30 @@
 <template>
 <div>
-  <h1>报名详情</h1>
-    <li
+  <h1 class="header1">博雅学堂| 粤剧第一课</h1>
+  <img class="logo" src="https://i0.hdslb.com/bfs/album/edd48c060074066c8d30c847052b091c869a3cb5.jpg@.webp" alt="1.">
+  <h3 class="header3">活动时间：1月1日 上午8:00-11:30</h3>
+  <img class="logo" src="https://i0.hdslb.com/bfs/album/ac6762e4a063b890ad8c56de4a9c879c81308a08.jpg@.webp" alt="2.">
+  <h3 class="header3">活动地点：D5d 201</h3>
+  <img class="logo" src="https://i0.hdslb.com/bfs/album/1304e90f969c07a4e5adbd563156675b20abd4ce.jpg@.webp" alt="3.">
+  <h3 class="header3">报名时间：12月31日中午12时至晚6时</h3>
+  <img class="logo" src="https://i0.hdslb.com/bfs/album/e97ae444015e9d04a1bf4ee1a7dacdabcc13e81b.jpg@.webp" alt="4.">
+  <h3 class="header3">主讲嘉宾：xxx</h3>
+  <h2 class="header2">活动介绍</h2>
+  <h4 class="greyline"></h4>
+  <div>
+  <h3 class="para_title">粤剧第一课：走进粤剧</h3>
+  <p class="para">粤剧于2009年列入人类非物质文化遗产代表作名录，如今，粤剧入选“世遗”已步入第二个十年。11月29日又将迎来一年一度的“粤剧日”，从粤剧节期间的多项活动以及多方反馈来看，新时期粤剧的传承与发展始终是人们关注的话题。相关部门和业界也在努力探索，希望引领人们一起走进粤剧的百花深处。</p>
+  <img class="pic" src="https://img1.baidu.com/it/u=1568875690,243587937&fm=253&fmt=auto&app=138&f=JPEG?w=742&h=500">
+  </div>
+  <h2 class="header2">活动评价</h2>
+  <h4 class="greyline"></h4>
+  <div>
+    <img class="profile_photo" src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fc-ssl.duitang.com%2Fuploads%2Fitem%2F202006%2F25%2F20200625025317_JUSUj.jpeg&refer=http%3A%2F%2Fc-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1646640037&t=f4c67e6ea720424b3e5868d5ad711fc7">
+    <h3 class="profile_name">江承儒</h3>
+    <img class="ranking" src="https://img0.baidu.com/it/u=2266172358,1984986812&fm=253&fmt=auto&app=138&f=JPG?w=667&h=500">
+    <p class="comment">好，很好，非常好，明天会更好</p>
+  </div>
+    <li class="clickbutton"
   @click="go('/pages/activity_join_form/activity_join_form')">立即报名</li>
 </div>
 </template>
@@ -25,3 +48,130 @@ export default {
   }
 }
 </script>
+<style>
+.logo{
+margin-left: 5%;
+margin-top: 5%;
+width: 20px;
+height: 20px;
+line-height: 20px;
+text-align: center;
+}
+.header1{
+margin-left: 5%;
+top: 87px;
+width: 350px;
+height: 36px;
+color: rgba(16, 16, 16, 100);
+font-size: 28px;
+text-align: left;
+font-family: PMZDBiaoTi-regular;
+}
+.header2{
+margin-left: 5%;
+top: 453px;
+width: 80px;
+height: 30px;
+color: rgba(16, 16, 16, 100);
+font-size: 20px;
+text-align: left;
+font-family: SourceHanSansSC-black;
+}
+.header3{
+margin-left: 10%;
+margin-top: -5%;
+width: 300px;
+height: 24px;
+color: rgba(16, 16, 16, 100);
+font-size: 14px;
+text-align: left;
+font-family: PingFangSC-regular;
+}
+.para_title{
+  margin-left: 5%;
+top: 142px;
+width: 300px;
+height: 28px;
+color: rgba(16, 16, 16, 100);
+font-size: 18px;
+text-align: left;
+font-family: PingFangSC-regular;
+}
+.para{
+  margin-left: 5%;
+top: 142px;
+width: 300px;
+height: 24px;
+color: rgba(16, 16, 16, 100);
+font-size: 14px;
+text-align: left;
+font-family: PingFangSC-regular;
+}
+.greyline{
+  margin-left: 5%;
+top: 688px;
+width: 313px;
+height: NaNpx;
+border: 1px solid rgba(187, 187, 187, 100);
+}
+.clickbutton{
+margin-left: 30%;
+top: 901px;
+padding-top: 5%;
+width: 125px;
+height: 50px;
+line-height: 29px;
+border-radius: 20px 20px 20px 20px;
+color: rgba(16, 16, 16, 100);
+font-size: 20px;
+text-align: center;
+box-shadow: 0px 2px 6px 0px rgba(0, 0, 0, 0.4);
+font-family: Arial;
+border: 4px solid rgba(245, 34, 45, 100);
+}
+.pic{
+margin-left: 5%;
+margin-top: 28%;
+width: 295px;
+height: 169px;
+}
+.profile_photo{
+margin-left: 3%;
+margin-top: 3%;
+width: 35px;
+height: 39px;
+line-height: 20px;
+text-align: center;
+border: 1px solid rgba(187, 187, 187, 100);
+}
+.profile_name{
+margin-left: 15%;
+margin-top: -12%;
+width: 42px;
+height: 24px;
+color: rgba(16, 16, 16, 100);
+font-size: 14px;
+text-align: left;
+font-family: PingFangSC-regular;
+}
+.ranking{
+margin-left: 15%;
+margin-top: -3%;
+width: 21px;
+height: 16px;
+line-height: 20px;
+background-color: rgba(238, 145, 150, 100);
+text-align: center;
+border: 1px solid rgba(187, 187, 187, 100);
+}
+.comment{
+margin-left: 5%;
+margin-top: 3%;
+width: 200px;
+height: 24px;
+color: rgba(16, 16, 16, 100);
+font-size: 14px;
+text-align: left;
+font-family: PingFangSC-regular;
+}
+</style>
--- src/pages/activity_join_form/activity_join_form.vue
+++ src/pages/activity_join_form/activity_join_form.vue
@@ -1,6 +1,33 @@
 <template>
 <div>
-  <h1>报名表单</h1>
+  <h1 class="header1">填写报名信息</h1>
+  <form action="#">
+    <h2 class="header2">1.姓名</h2>
+    <input class="bracket" type="text" placeholder="请在此填入您的姓名" required>
+  <view class="page-body">
+    <view class="page-section">
+      <text class="header2">2.学院</text>
+      <view>
+        <picker mode="selector" :range="selector" @change="onChange">
+          <view class="picker">
+            请在此选择您的学院：{{selectorChecked}}
+          </view>
+        </picker>
+      </view>
+    </view>
+  </view>
+    <h2 class="header2">3.学号</h2>
+    <input class="bracket" type="text" placeholder="请在此填入您的学号" required>
+    <h2 class="header2">4.专业及班级</h2>
+    <input class="bracket" type="text" placeholder="请在此填入您的专业及班级" required>
+    <h2 class="header2">5.联系方式</h2>
+    <input class="bracket" type="text" placeholder="请在此填入您的电话号码" required>
+    <h2 class="header2">6.请再次确认联系方式</h2>
+    <input class="bracket" type="text" placeholder="请在此重新填入您的电话号码" required>
+    <h2 class="header2">7.身份证号码</h2>
+    <input class="bracket" type="text" placeholder="请在此填入您的身份证号码" required>
+    <button class="button" type="submit">提交</button>
+  </form>
 </div>
 </template>
  
@@ -9,16 +36,76 @@ import {ref} from 'vue'
 import Taro from '@tarojs/taro'
 
 export default {
-  setup(props, context) {
-    // data
-    // method
-    const go = (url) => {
-      Taro.navigateTo({url})
-    }
-
-    return {
-      go,
+    data() {
+      return {
+        selector: ['铭诚书院', '峻德书院', '生物医学科学与工程学院（研究生）', '吴贤铭智能工程学院（研究生）', '分子科学与工程学院（研究生）', '微电子学院（研究生）', '材料科学与工程学院', '工商管理学院', '公共管理学院', '外国语学院', '马克思主义学院', '计算机科学与工程学院', '软件学院', '环境与能源学院', '生物科学与与工程学院', '新闻与传播学院', '设计学院', '法学院（知识产权学院）', '经济与金融学院', '电子商务系', '旅游管理系', '中澳班', '医学院', '其他学院'],
+      }
+    },
+    methods: {
+      onChange: function(e) {
+        this.selectorChecked = this.selector[e.detail.value]
+      }
     }
   }
-}
 </script>
+<style lang="scss">
+.header1{
+margin-left: 5%;
+top: 117px;
+width: 200px;
+height: 30px;
+color: rgba(16, 16, 16, 100);
+font-size: 20px;
+text-align: left;
+font-family: PMZDBiaoTi-regular;
+}
+.bracket{
+margin-left: 5%;
+top: 271px;
+width: 241px;
+height: 28px;
+line-height: 20px;
+border-radius: 3px;
+background-color: rgba(255, 255, 255, 100);
+color: rgba(193, 194, 197, 100);
+font-size: 14px;
+text-align: left;
+font-family: Arial;
+border: 1px solid rgba(220, 224, 231, 100);
+}
+.button{
+margin-left: 30%;
+margin-top: 10%;
+padding-top: 3%;
+width: 136px;
+height: 48px;
+line-height: 26px;
+border-radius: 3px;
+background-color: rgba(251, 104, 104, 100);
+color: rgba(255, 255, 255, 100);
+font-size: 18px;
+text-align: center;
+font-family: Arial;
+}
+.header2{
+margin-left: 5%;
+top: 392px;
+width: 150px;
+height: 20px;
+color: rgba(16, 16, 16, 100);
+font-size: 14px;
+text-align: left;
+font-family: SourceHanSansSC-regular;
+}
+.picker{
+margin-left: 5%;
+top: 340px;
+width: 241px;
+height: 31px;
+background-color: rgba(255, 255, 255, 100);
+color: rgba(65, 80, 88, 100);
+font-size: 14px;
+text-align: left;
+font-family: Microsoft Yahei;
+}
+</style>
